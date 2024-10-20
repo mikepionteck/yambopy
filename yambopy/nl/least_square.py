7a8d567c0733e88e8a912bc582e8be837576d8eb
Mike Pionteck <mikenico1995@aol.com>
2024-10-20 14:30:20 +0200

    Regularization for all

--- yambopy/nl/least_square.py
+++ yambopy/nl/least_square.py
@@ -68,9 +68,9 @@ def LS_fit_diff(c,order,f1,f2,t,s):
 def LS_fit_diff_ridge(c,order,f1,f2,t,s,lambda_ridge):
     residual = LS_fit_diff(c,order,f1,f2,t,s)
     ridge = 0
-    diff = abs(f1*ha2ev-f2*ha2ev)
-    if diff < 1e-2:
-        ridge = lambda_ridge
+    #diff = abs(f1*ha2ev-f2*ha2ev)
+    #if diff < 1e-2:
+    ridge = lambda_ridge
     reg_term = ridge * c
     res = np.concatenate((residual, reg_term))
     return res
